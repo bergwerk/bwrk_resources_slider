681bda7aa4d7d77fc4d90d5afa0de6de7846bf97
Georg Dümmler <gd@bergwerk.ag>
2016-01-14 10:27:09 +0100

    init

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'BERGWERK Resources Slider',
+	'description' => 'Renders page media resources for slide use.',
+	'category' => 'plugin',
+	'author' => 'BERGWERK[GD]',
+	'author_email' => 'gd@bergwerk.ag',
+	'author_company' => 'www.bergwerk.ag',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
