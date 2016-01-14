aec7dd53256a09bef1d04c5ce71dfce1212cb6f2
Georg Dümmler <gd@bergwerk.ag>
2016-01-14 10:28:29 +0100

    create viewcontroller

---
+++ Classes/Controller/ViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BERGWERK\BwrkResourcesSlider\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ViewController extends ActionController
+{
+    public function indexAction()
+    {
+
+    }
+}
