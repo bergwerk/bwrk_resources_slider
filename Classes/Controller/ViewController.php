9bb58d2a8ea3768c54eff986ce3334014fe34444
Georg Dümmler <gd@bergwerk.ag>
2016-01-14 10:40:13 +0100

    add ViewController

--- Classes/Controller/ViewController.php
+++ Classes/Controller/ViewController.php
@@ -3,9 +3,28 @@
 namespace BERGWERK\BwrkResourcesSlider\Controller;
 
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 class ViewController extends ActionController
 {
+    /**
+     * @var \BERGWERK\BwrkResourcesSlider\Domain\Repository\PageRepository
+     * @inject
+     */
+    protected $pageRepository;
+
+    /**
+     * @var int
+     */
+    protected $pid;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->pid = $GLOBALS['TSFE']->id;
+    }
+
     public function indexAction()
     {
 
